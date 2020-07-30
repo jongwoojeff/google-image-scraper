cd824ecdc1b7dc08818959213d5e19895c97f19b
unknown <jongwoo2@illinois.edu>
2020-07-30 22:45:59 +0900

    duplicate image problem not solved

--- test.py
+++ test.py
@@ -19,26 +19,49 @@ print("opening URL")
 # image_cnt = int(input())
 # print("Getting " + str(image_cnt) + " images...")
 # path for mac
-# driver = webdriver.Chrome("./chromedriver")
-driver = webdriver.Chrome("/Users/jeff/Desktop/chromedriver")
+driver = webdriver.Chrome("./chromedriver")
+# driver = webdriver.Chrome("/Users/jeff/Desktop/chromedriver")
 driver.get(url)
 
 element = driver.find_element_by_tag_name("body")
 image_urls=[]
-imgurl = driver.find_element_by_xpath('//div//div//div//div//div//div//div//div//div//div[%s]//a[1]//div[1]//img[1]'%(str(1)))
-imgurl.click()
-images = driver.find_elements_by_class_name("n3VNCb")
+# imgurl = driver.find_element_by_xpath('//div//div//div//div//div//div//div//div//div//div[%s]//a[1]//div[1]//img[1]'%(str(7)))
+# imgurl.click()
+# images = driver.find_elements_by_class_name("n3VNCb")
 
-for image in images:
+for i in range(1,4):
+    imgurl = driver.find_element_by_xpath('//div//div//div//div//div//div//div//div//div//div[%s]//a[1]//div[1]//img[1]'%(str(i)))
+    imgurl.click()
+
+    #select image from the popup
+    time.sleep(3)
+    print(i)
+    images = driver.find_elements_by_class_name("n3VNCb")
+    for image in images:
     # print(image.get_attribute("src"))
-    image_urls.append(image.get_attribute("src"))
-image = image_urls[0]
+        image_urls.append(image.get_attribute("src"))
 
+
+# for image in images:
+#     # print(image.get_attribute("src"))
+#     image_urls.append(image.get_attribute("src"))
+# image = image_urls[0]
+print(len(image_urls))
 def test_single(url):
     try:
         file_path = dir_path + "/" + "newTest" + ".jpg"
         urllib.request.urlretrieve(url, file_path)
     except Exception as e:
         print ("failed")
-test_single(image)
+
+def test_multiple(urls):
+    for i in range(len(urls)):
+        try:
+            file_path = dir_path + "/" + "test" + str(i) + ".jpg"
+            urllib.request.urlretrieve(urls[i], file_path)
+        except Exception as e:
+            print ("failed at: " + str(i))
+# test_single(image)
 # print(image_urls[0])
+image_urls = list(dict.fromkeys(image_urls))
+test_multiple(image_urls)
